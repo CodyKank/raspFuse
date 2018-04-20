6852b2a9b10cfa6ae983913ca2b90b8292337997
jonathan-wideman <jonathan.m.wideman@gmail.com>
2018-04-20 12:11:10 -0400

    Harvest bits updates geigercpm with counts per minute
    - cpm is based on a running average of the last 30 geiger event times
    - harvest bits outputs the random bytes to geigerRandCache instead of geigerbits
    - improved formatting of the cpm log output from fuse write geiger

--- randfs.py
+++ randfs.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 from __future__ import with_statement
-from time import time
+from time import time, localtime, strftime
 
 import os
 import sys
@@ -168,10 +168,11 @@ class Passthrough(Operations):
         return os.read(fh, length)
 
     def write(self, path, buf, offset, fh):
-        geigercpmStr = ""
+        geigercpmStr = "Counts per minute at " + strftime("%H:%M:%S", localtime(time())) + " was "
         if path == "/geiger":
             with open("geigercpm") as geigercpmFile:
-                geigercpmStr = geigercpmStr + geigercpmFile.readline()
+                geigercpmStr += "{0:.2f}".format(float(geigercpmFile.readline())) + "\n"
+
             print geigercpmStr
         with open('geigerOut.txt', 'w') as dmpFile:
             dmpFile.write(geigercpmStr)
